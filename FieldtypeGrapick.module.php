e64a5d55a8fa76c43672c10f7b1c37e5f1108d5f
solonmedia <support@solon.media>
2024-01-23 08:57:01 -0600

    debug fieldtype

--- FieldtypeGrapick.module.php
+++ FieldtypeGrapick.module.php
@@ -13,7 +13,7 @@
 	public static function getModuleInfo() {
 		return array(
 			'title' => 'Grapick',
-			'version' => '1.0.5',
+			'version' => '1.0.6',
 			'author' => 'Jacob Gorny',
 			'href' => 'https://github.com/solonmedia/FieldtypeGrapick',
 			'summary' => 'Field that incorporates the Grapick javascript gradient designer and stores an array of 32-bit rgba colors, gradient positions and optionally gradient styles and a plaintext style rule for CSS.',
@@ -107,6 +107,8 @@
 	 */
 	public function getBlankValue(Page $page, Field $field) {
 		
+		$gradient->$this->wire('modules')->get('InputfieldGrapick');
+
 		$context = ($page && $page->id) ? $field->getContext($page->template) : $field;
 
 		if(!($page instanceof NullPage)) {
